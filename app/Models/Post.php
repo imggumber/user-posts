db82d8855f40ce97c5c7df91707bda6cbc51fd21
Jatin Gandhi <gandhijatin00@gmail.com>
2025-02-24 15:49:10 +0530

    Define relationship + fillables

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function users() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected $fillable = [
+        'title',
+        'user_id',
+    ];
 }
